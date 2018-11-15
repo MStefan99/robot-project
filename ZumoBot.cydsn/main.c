8ab3383fa186a9090f2929f72275a057c59adc65
msaveleva <mariiasaveleva@gmail.com>
2018-11-15 10:20:39 +0200

    received time from user

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -31,6 +31,7 @@ bool movement_allowed = false;
 volatile bool calibration_mode = false;
 
 CY_ISR_PROTO(Button_Interrupt);
+bool incorrect_time(int hours, int minutes);
 
 CY_ISR(Button_Interrupt)
 {
@@ -64,8 +65,31 @@ int zmain(void)
             continue;
         }
         
+        int hours = 0;
+        int minutes = 0;
+        printf("Welcome to Assignment 1 of Week 5. To continue program needs time in 24-hours format.\n");
         
+        do {
+            printf("Enter hours: ");
+            scanf("%d", &hours);
+            printf("Enter minutes: ");
+            scanf("%d", &minutes); 
+            
+            if (incorrect_time(hours, minutes)) {
+                printf("Incorrect data, try again.\n");
+            } else {
+                printf("You entered %d:%d\n", hours, minutes);
+            }
+        } while (incorrect_time(hours, minutes));
     }
+}
+
+bool incorrect_time(int hours, int minutes) 
+{
+    bool incorrect_hours = hours < 0 || hours > 23;
+    bool incorrect_minutes = minutes < 0 || minutes > 59; 
+   
+    return incorrect_hours || incorrect_minutes; 
 }
 
 /* [] END OF FILE */

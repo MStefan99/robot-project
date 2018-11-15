7fda8e4ff17f6da361fd866e10f238e51abf7c03
msaveleva <mariiasaveleva@gmail.com>
2018-11-15 10:00:16 +0200

    code cleanup

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -43,17 +43,6 @@ CY_ISR(Button_Interrupt)
 
 int zmain(void)
 {    
-    reflectance_offset_ reflectance_offset = {0,0,0};
-    struct sensors_ reflectance_values;
-    bool reflectance_black = false;
-    uint8_t cross_count = 0;
-    const uint8_t speed = 100;
-    int line_shift_change;
-    int line_shift;
-    int shift_correction;
-    uint8_t p_coefficient = 1; // TODO MStefan99: calibrate
-    uint8_t d_coefficient = 1; // TODO MStefan99: calibrate
-    
     CyGlobalIntEnable; /* Enable global interrupts. */
     Button_isr_StartEx(Button_Interrupt); // Link button interrupt to isr
     
@@ -75,48 +64,7 @@ int zmain(void)
             continue;
         }
         
-        if(!cross_detected()){
-            if(reflectance_black){
-                // Update cross count after leaving the intersection
-                ++cross_count;
-            }
-            reflectance_black = false;
-        } else {
-            reflectance_black = true;
-        }
         
-        if(calibration_mode){
-            reflectance_read(&reflectance_values);
-            reflectance_offset = reflectance_calibrate(&reflectance_values);
-            calibration_mode = false;
-        } 
-        
-        reflectance_read(&reflectance_values);
-        reflectance_normalize(&reflectance_values, &reflectance_offset);
-        
-        line_shift = get_offset(&reflectance_values);
-        line_shift_change = get_offset_change(&reflectance_values);        
-        shift_correction = line_shift * p_coefficient + line_shift_change * d_coefficient;
-        
-        print_mqtt("Zumo025/log",\
-            "Shift: %d\n"
-            "Sensor l3:%d\n"
-            "Sensor l2:%d\n"
-            "Sensor l1:%d\n",\
-        shift_correction, reflectance_values.l3, reflectance_values.l2, reflectance_values.l1);
-        print_mqtt("Zumo025/log",\
-            "Sensor r1:%d\n"
-            "Sensor r2:%d\n"
-            "Sensor r3:%d\n\n",\
-        reflectance_values.r1, reflectance_values.r2, reflectance_values.r3);
-        
-        if(movement_allowed){
-            if(cross_count > 3){
-                motor_forward(0,0);
-            } else {
-                motor_turn_diff(speed, shift_correction);
-            } 
-        }
     }
 }
 
